c7450af7d378cbbf61f1f426339e90ba8bc6ae54
UditNarayanPagad <uditnarayanpagad@gmailcom>
2024-07-17 19:57:18 +0530

    changes

--- Data/index.ts
+++ Data/index.ts
@@ -13,7 +13,7 @@ export const navItems = [
       className: "lg:h-[72vh] h-[72vh] lg:w-[40vw]",
       imgClassName: "w-full h-full bg-cover rounded-lg",
       titleClassName: "",
-      img: "/Udit.jpg",
+      img: "./Udit.jpg",
       spareImg: "",
     },
     {
@@ -47,8 +47,8 @@ export const navItems = [
       className: "absolute  h-[40vh] lg:top-[65vh] lg:right-0 bottom-8 lg:w-[40vw]",
       // imgClassName: "cover",
       titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
+      img: "./b5.svg",
+      spareImg: "./grid.svg",
     },
   ];
   
@@ -57,38 +57,38 @@ export const navItems = [
       title: "Wanderlust functional App",
         description: "Features secure user authentication, allowing users to create accounts, save travel itineraries, and share experiences. Implements dynamic, responsive design and real-time data updates for a seamless user experience.",
       link: "https://github.com/UditNarayanPagad/Wanderlust-TravelWeb-Full-Stack",
-      url:"/wanderlust.png",
+      url:"./wanderlust.png",
     },
     {
       title: "Cuberto Animated WebApp",
         description: "A fully animated website featuring smooth scrolling and a dynamic cursor for an immersive user experience. Showcases advanced animations and transitions to create an engaging, interactive interface",
       link: "https://github.com/UditNarayanPagad/Cuberto--Animated-",
-      url:"/cuberto.png",
+      url:"./cuberto.png",
     },
     {
       title: "HiLink fully responsive",
         description: "A fully responsive travel webapp built with Next.js, offering a seamless and dynamic user experience. Features intuitive navigation, optimized performance, and mobile-friendly design to cater to travelers on the go.",
       link: "https://github.com/UditNarayanPagad/Hilink-travel-app",
-      url:"/TravelApp.png",
+      url:"./TravelApp.png",
     },
     {
       title: "Animated Fanta 3D Website",
       description: "An immersive 3D website experience that brings the vibrant world of Fanta to life. Utilizes advanced Gsap and CSS animations to create dynamic, interactive visuals. Designed to captivate users with engaging, fluid animations interface.",
       link: "https://github.com/UditNarayanPagad/Fanta-Website",
-      url:"/fanta.png",
+      url:"./fanta.png",
     },
     {
       title: "Refokus React App",
       description:
         "An advanced animated website built with GSAP and React.js, delivering a highly interactive and engaging user experience. Leverages the power of GSAP to create smooth, captivating animations and transitions.",
       link: "https://github.com/UditNarayanPagad/Refokus",
-      url:"/refokus.png",
+      url:"./refokus.png",
     },
     {
       title: "Pintrest WebApp",
       description:
         "A fully functional web app developed with Express.js, featuring robust authentication and image upload capabilities. Allows users to securely create accounts, log in.",
       link: "https://github.com/UditNarayanPagad/Pintrest-Clone-FullStack-MERN--Application",
-      url:"/pintrest.png",
+      url:"./pintrest.png",
     },
   ];
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {images: {
   domains: [
     "api.microlink.io", // Microlink Image Preview
   ],
-},};
+},
+output:"export",
+};
 
 export default nextConfig;
