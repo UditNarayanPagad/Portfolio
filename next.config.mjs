d6e7967794da9c5112168bef75b888d4c0c06339
UditNarayanPagad <uditnarayanpagad@gmailcom>
2024-07-19 19:33:27 +0530

    ui changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { images: { unoptimized: true } };
-// const nextConfig = {images: {
-//   domains: [
-//     "api.microlink.io", // Microlink Image Preview
-//   ],
-// },
-// output:"export",
-// };
+// const nextConfig = { images: { unoptimized: true } };
+const nextConfig = {images: {
+  domains: [
+    "api.microlink.io", // Microlink Image Preview
+  ],
+},
+output:"export",
+};
 
 export default nextConfig;
